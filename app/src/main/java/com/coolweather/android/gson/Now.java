666f5f5f0ef40332c8291b51c00d7c1024bc3522
wang0232103 <wangtaosi@qq.com>
2017-12-04 01:44:19 +0800

    有个bug，奇怪了。

--- app/src/main/java/com/coolweather/android/gson/Now.java
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -10,6 +10,9 @@ public class Now {
     @SerializedName("tmp")
     public String temperature;
 
+    @SerializedName("cond")
+    public More more;
+
     public class More{
         @SerializedName("text")
         public String info;
